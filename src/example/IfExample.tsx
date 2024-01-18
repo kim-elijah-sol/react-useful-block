eb5486d7b285012cc5c04edaef9702ebe5056552
kim-elijah-sol <kimelijahsol@gmail.com>
2024-01-18 18:02:49 +0900

    feat: [If] Exmple > ElseIf 작성

--- src/example/IfExample.tsx
+++ src/example/IfExample.tsx
@@ -1,14 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import If from "../lib/ReactUsefulBlock/If";
 
 function IfExample() {
-  const [value, setValue] = useState<number | null>(null);
+  const [value, setValue] = useState<number | string | null>(null);
+
+  useEffect(() => {
+    if (typeof value === "number" && value >= 10) {
+      setValue("ten");
+    }
+  }, [value]);
 
   return (
     <div>
       <h1>If Example</h1>
 
-      <If condition={value !== null}>
+      <If condition={typeof value === "number"}>
         <If.Then>
           {/**
            * @description
@@ -20,13 +26,22 @@ function IfExample() {
               <>
                 <p>value is Not Null</p>
                 <p>value:{value!.toString()}</p>
-                <button onClick={() => setValue(value! + 1)}>increment</button>
+                <button onClick={() => setValue((value as number)! + 1)}>
+                  increment
+                </button>
                 <br />
                 <button onClick={() => setValue(null)}>set Null</button>
               </>
             );
           }}
         </If.Then>
+        <If.ElseIf condition={typeof value === "string"}>
+          <p>value is String</p>
+
+          <button onClick={() => setValue(null)}>
+            <span>set Null</span>
+          </button>
+        </If.ElseIf>
         <If.Else>
           <p>value is Null</p>
 
